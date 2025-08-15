c30ee38f62886dedf661916d528625bb13b069d5
P . A Ishara Deshan Appuhami <110235551+IsharaDeshan6@users.noreply.github.com>
2025-08-15 21:20:17 +0530

    AXSSR-29 feat: add PredictorDisasters component with basic layout

---
+++ packages/mobile/app/home/PredictorDisasters.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PredictorDisasters = () => {
+  return (
+    <View>
+      <Text>PredictorDisasters</Text>
+    </View>
+  );
+};
+
+export default PredictorDisasters;
